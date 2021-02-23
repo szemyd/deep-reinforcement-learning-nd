2c208052dcef2826a429a72dec46c3a808babf95
Daniel Szemerey <szemy2@gmail.com>
2021-02-23 09:17:11 +0100

    feat: Created agent for MiniTaxi-v3. Best score: 8.93
    The performance needs to be improved.
    
    Related: -

--- lab-taxi/agent.py
+++ lab-taxi/agent.py
@@ -1,5 +1,6 @@
 import numpy as np
 from collections import defaultdict
+import random
 
 class Agent:
 
@@ -12,6 +13,14 @@ class Agent:
         """
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
+        
+        self.alpha = 0.1
+        self.gamma = 0.9999
+
+        self.eps_start = 1.0
+        self.eps_decay=.9999
+        self.eps_min=0.0001
+        self.epsilon = self.eps_start
 
     def select_action(self, state):
         """ Given the state, select an action.
@@ -24,7 +33,25 @@ class Agent:
         =======
         - action: an integer, compatible with the task's action space
         """
-        return np.random.choice(self.nA)
+
+        def epsilon_greedy(Q, state, nA, eps):
+            """Selects epsilon-greedy action for supplied state.
+
+            Params
+            ======
+                Q (dictionary): action-value function
+                state (int): current state
+                nA (int): number actions in the environment
+                eps (float): epsilon
+            """
+            if random.random() > eps: # select greedy action with probability epsilon
+                return np.argmax(Q[state])
+            else:                     # otherwise, select an action randomly
+                return random.choice(np.arange(nA))
+
+        self.epsilon = max(self.epsilon * self.eps_decay, self.eps_min)
+
+        return epsilon_greedy(self.Q, state, self.nA, self.epsilon)
 
     def step(self, state, action, reward, next_state, done):
         """ Update the agent's knowledge, using the most recently sampled tuple.
@@ -37,4 +64,38 @@ class Agent:
         - next_state: the current state of the environment
         - done: whether the episode is complete (True or False)
         """
-        self.Q[state][action] += 1
+        
+
+        def epsilon_greedy_prob_return(nA, epsilon, Q, s_1):
+            all_prob = np.ones(nA) * epsilon/nA
+            prob_above_epsilon = (1 - epsilon) + (epsilon/nA) ## greedy choice
+
+
+            greedy_action = np.argmax(Q[s_1])
+            all_prob[greedy_action] = prob_above_epsilon
+
+            # prob_adj_value_best = prob_above_epsilon * Q[s_1][greedy_action]
+            # prob_adj_value_rest = np.dot(Q[s_1], prob_below_epsilon )
+            
+            # print(prob_adj_value_best)
+            # print(prob_adj_value_rest)
+
+            return np.dot(Q[s_1], all_prob )
+            
+
+        def update_expected_Q(alpha, gamma, Q, s_0, a_0, r_1, epsilon, nA, s_1 = None):
+            # new_Q = copy.deepcopy(Q)
+            Qs_next  = epsilon_greedy_prob_return(nA, epsilon, Q, s_1) if s_1 is not None else 0  
+
+            current_return = Q[s_0][a_0]
+            target_return = r_1 + (gamma * Qs_next)
+            
+            difference = (target_return - current_return)
+            
+            # new_Q[s_0][a_0] = current_return + (alpha * difference)
+            # return new_Q
+            return current_return + (alpha * difference)
+
+        self.Q[state][action] = update_expected_Q(self.alpha, self.gamma, self.Q, state, action, reward, self.epsilon, self.nA, next_state )
+        
+        
