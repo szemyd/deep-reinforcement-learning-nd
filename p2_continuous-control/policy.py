9ad6043dd8cd8ed6a4503ee9db8b8708480a657e
Daniel Szemerey <szemy2@gmail.com>
2021-04-03 12:46:38 +0200

    feat: Started implementing DDPG for Continous Control problem
    Project 2 for Nanodegree.
    
    Related: -

---
+++ p2_continuous-control/policy.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Actor(nn.Module):
+    def __init__(self, state_space, action_space, hidden_layer_param = [50, 50]):
+
+        self.fc_in = nn.Linear(state_space, hidden_layer_param[0])
+        self.hidden_layers = [nn.Linear(hidden_layer_param[i], hidden_layer_param[i+1]) for i in range(len(hidden_layer_param)-1)]
+        self.fc_out = nn.Linear(hidden_layer_param[-1], action_space)
+
+        self.activation = nn.ReLU()
+
+
+    def forward(self, state):
+        x = state.view(-1)
+        x = self.activation(self.fc_in(x))
+
+        for hidden_layer in self.hidden_layers:
+            x = activation(hidden_layer(x))
+
+        x = self.activation(self.fc_out) 
+
+        return F.tanh(x)
+
+class Critic(nn.Module):
+    def __init__(self, state_space, action_space, hidden_layer_param = [50, 50]):
+
+        self.fc_in = nn.Linear(state_space, hidden_layer_param[0])
+        self.hidden_layers = [nn.Linear(hidden_layer_param[i], hidden_layer_param[i+1]) for i in range(len(hidden_layer_param)-1)]
+        self.fc_out = nn.Linear(hidden_layer_param[-1], action_space)
+
+        self.activation = nn.ReLU()
+
+    def forward(self, state):
+        x = state.view(-1)
+        x = self.activation(self.fc_in(x))
+
+        for hidden_layer in self.hidden_layers:
+            x = activation(hidden_layer(x))
+
+        x = self.activation(self.fc_out) 
+
+        return F.tanh(x)
